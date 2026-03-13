d6fdb3bb22cbd96b9efcdc1a84ae43b5ae4e3df5
Diego Vàsquez <Alejandrovasq0803gmail.com>
2026-03-12 18:47:41 -0600

    update samnu version

--- website/server/script.js
+++ website/server/script.js
@@ -1,24 +1,27 @@
 function forzarDescarga(url) {
-    const link = document.createElement('a');
-    link.href = url;
-    link.setAttribute('download', '');
-    link.style.display = 'none';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+  const link = document.createElement("a");
+  link.href = url;
+  link.setAttribute("download", "");
+  link.style.display = "none";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 }
 
-document.getElementById('btn-android').addEventListener('click', () => {
-    const url = 'https://raw.githubusercontent.com/Trebol4Devop/SAMNU/main/SamnuAndroid/Samnu.apk';
-    forzarDescarga(url);
+document.getElementById("btn-android").addEventListener("click", () => {
+  const url =
+    "https://github.com/Trebol4Devop/SAMNU/releases/download/v1.1.2/SAMNU-Android.apk";
+  forzarDescarga(url);
 });
 
-document.getElementById('btn-windows').addEventListener('click', () => {
-    const url = 'https://raw.githubusercontent.com/Trebol4Devop/SAMNU/main/SamnuWindows/Samnu.rar';
-    forzarDescarga(url);
+document.getElementById("btn-windows").addEventListener("click", () => {
+  const url =
+    "https://github.com/Trebol4Devop/SAMNU/releases/download/v1.1.2/SAMNU-Windows.zip";
+  forzarDescarga(url);
 });
 
-document.getElementById('btn-manual').addEventListener('click', () => {
-    const url = 'https://raw.githubusercontent.com/Trebol4Devop/SAMNU/main/Manual%20de%20Usuario%20-%20SAMNU%20v1.0.0.pdf';
-    forzarDescarga(url);
+document.getElementById("btn-manual").addEventListener("click", () => {
+  const url =
+    "https://raw.githubusercontent.com/Trebol4Devop/SAMNU/main/Manual%20de%20Usuario%20-%20SAMNU%20v1.0.0.pdf";
+  forzarDescarga(url);
 });
